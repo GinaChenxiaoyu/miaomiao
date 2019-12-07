ccc0bd8191f82587c964083483e282581220d02a
GinaChenxiaoyu <13121087598@163.com>
2019-12-07 22:01:29 +0800

    add getCity

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -43,7 +43,8 @@
       message(newVal){
         var that=this;
         this.cancelRequest();
-        this.axios.get('/api/searchList?cityId=10&kw='+newVal,{
+        var cityId=this.$store.state.city.id;
+        this.axios.get('/api/searchList?cityId='+cityId+'&kw='+newVal,{
           cancelToken: new this.axios.CancelToken(function (c) {
             that.source = c;
           }
